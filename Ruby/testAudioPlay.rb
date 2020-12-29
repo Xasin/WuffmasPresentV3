859127440bcb66bbab0dbdc8654fece7bbcde95f
Xasin <davidbailey.2889@gmail.com>
2020-12-29 21:50:40 +0100

    Forgot to update this

--- Ruby/testAudioPlay.rb
+++ Ruby/testAudioPlay.rb
@@ -7,33 +7,54 @@ require_relative 'CassetteSource.rb'
 
 require 'mqtt/sub_handler.rb'
 
-$mqtt = MQTT::SubHandler.new('mqtt://JUNNpEz4Z33vMbGZbB5l6cujsQebJaVjVxU81h2QaWcKXGAW8t72ts9L6cKGauNV@mqtt.flespi.io');
+#$mqtt = MQTT::SubHandler.new('mqtt://JUNNpEz4Z33vMbGZbB5l6cujsQebJaVjVxU81h2QaWcKXGAW8t72ts9L6cKGauNV@mqtt.flespi.io');
+$mqtt = MQTT::SubHandler.new('mqtt://192.168.178.230');
 
 $wuff = Wuff::WuffCorder.new($mqtt, "84.CC.A8.0A.33.98");
 
 
-cassette = Xasin::OpusStream::CassetteSource.new();
-$wuff.audio_stream << cassette;
+$cassette = Xasin::OpusStream::CassetteSource.new();
+$wuff.audio_stream << $cassette;
 
-$wuff.state = :IDLE_PENDING;
+$wuff.state = :IDLE;
 
-source = Xasin::OpusStream::FileSource.new('ob-la-di.mp3')
-source.volume = 0.1
-source.pause
+def add_new_source(file = "/tmp/WuffRecord-84.CC.A8.0A.33.98-temp.ogg")
+	unless $file_source.nil?
+		$file_source.stop()
+	end
 
-$mqtt.subscribe_to "Wuffcorder/84.CC.A8.0A.33.98/BTN" do
-	if source.state == :paused
-		$wuff.state = :PLAYING
-		source.resume
-	elsif source.state == :playing
-		source.pause
+	$file_source = Xasin::OpusStream::FileSource.new(file);
+	$file_source.volume = 0.2;
+
+	$file_source.on_finish do
+		$file_source = nil;
+		$wuff.state = :IDLE
+	end
+
+	$cassette << $file_source;
+	$wuff.state = :PLAYING
+end
+
+$mqtt.subscribe_to "Wuffcorder/84.CC.A8.0A.33.9/BTN" do |btn|
+	case btn
+	when "PLAY"
+		if($file_source.nil?)
+			add_new_source
+		elsif $file_source.state == :paused
+			$wuff.state = :PLAYING
+			$file_source.resume
+		elsif $file_source.state == :playing
+			$file_source.pause
+		end
+	when "REC"
+		if($wuff.state == :RECORDING)
+			$wuff.stop_recording
+			$wuff.state = :IDLE_PENDING
+		else
+			$wuff.start_recording
+			$file_source&.pause
+		end
 	end
 end
-
-source.on_finish do
-	$wuff.state = :IDLE;
-end
-
-cassette << source
 
 Thread.stop()
